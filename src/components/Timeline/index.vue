21e96e02626830a079444ef143f8ac2a63c964fe
PoliZyh <272798802@qq.com>
2023-10-19 16:24:29 +0800

    feat: 完成项目动态面板

--- src/components/Timeline/index.vue
+++ src/components/Timeline/index.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="timeline-box">
+        
 
+        <el-collapse>
+            <el-collapse-item v-for="item in props.data" :key="item.date" :name="item.date">
+                <template #title>
+                    <h4 class="date">{{ item.date }}</h4>
+                </template>
+                <el-timeline>
+                    <el-timeline-item :timestamp="det.time" placement="top" v-for="det in item.more" :key="det.time">
+                        <el-card>
+                            <h4>{{ det.detail }}</h4>
+                            <p>{{ det.user }} committed {{ item.date }} {{ det.time }}</p>
+                        </el-card>
+                    </el-timeline-item>
+                </el-timeline>
+            </el-collapse-item>
+        </el-collapse>
     </div>
 </template>
 
@@ -8,7 +24,40 @@
 
 <script setup>
 
+
+const props = defineProps({
+    data: {
+        default: () => []
+    }
+})
+
 </script>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.timeline-box {
+    .date {
+        background-color: rgba(228, 228, 228, 0.641);
+        padding: 0px 10px;
+        width: fit-content;
+        border-radius: 4px;
+    }
+}
+</style>
+
+
+<style lang="scss">
+.timeline-box {
+    .el-collapse-item__header {
+        height: 60px;
+        padding-left: 10px;
+        padding: 10px;
+        background-color: #f7f8fa;
+        border: none;
+    }
+    .el-collapse-item__content {
+        padding: 20px;
+    }
+}
+
+</style>
--- src/views/ProjectDynamics/index.vue
+++ src/views/ProjectDynamics/index.vue
@@ -1,13 +1,56 @@
 <template>
-    <div>
-        pd
+    <div class="project-dynamics-page">
+        <h4>项目动态</h4>
+        <Timeline :data="data"></Timeline>
     </div>
 </template>
 
 <script setup>
+import Timeline from '@/components/Timeline/index.vue'
+import { ref } from 'vue';
 
+const data = ref([
+    {
+        date: '2023-10-15',
+        more: [
+            {
+                user: 'Ethan',
+                detail: '保存变量库文件[conn]',
+                time: '18:35'
+            },
+            {
+                user: 'Ethan',
+                detail: '保存变量库文件[conn]',
+                time: '18:35'
+            }
+        ]
+        
+    },
+    {
+        date: '2023-10-16',
+        more: [
+            {
+                user: 'Ethan',
+                detail: '保存变量库文件[conn]',
+                time: '18:35'
+            },
+            {
+                user: 'Ethan',
+                detail: '保存变量库文件[conn]',
+                time: '18:35'
+            }
+        ]
+        
+    }
+])
 </script>
 
 <style scoped lang="scss">
-
+.project-dynamics-page {
+    width: 100%;
+    height: 100%;
+    h4 {
+        margin-bottom: 20px;
+    }
+}
 </style>
