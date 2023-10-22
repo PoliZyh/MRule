76501ca094f21dbd40bcda991ea8ff8653269eb7
PoliZyh <272798802@qq.com>
2023-10-22 19:24:30 +0800

    feat: 文件树添加文件

---
+++ src/components/FileTree/components/TreeContextMenu.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="tree-context-menu-box" v-show="isShow" :style="style">
+        <div class="ctx-item" @click="handleCreateFolder('文件夹')" v-show="activeNode.type === 'folder'">新建文件夹</div>
+        <div class="ctx-item" @click="handleCreateFile('文件')" v-show="activeNode.type === 'folder'">新建文件</div>
+        <div class="ctx-item" @click="handleDelete()" >删除</div>
+    </div>
+    <el-dialog v-model="isShowDialog" :title="'创建' + dialogTitle">
+        <el-form>
+            <el-form-item :label="'请输入' + dialogTitle + '名'">
+                <el-input v-model="inputName"></el-input>
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                确认
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+    isShow: {
+        default: false,
+        type: Boolean
+    },
+    pos: {
+        default: () => {},
+    },
+    activeNode: {
+        default: () => ({ 
+            'type': 'folder'
+        })
+    }
+})
+const emits = defineEmits(['update:isShow'])
+const dialogTitle = ref('')
+const isShowDialog = ref(false)
+const inputName = ref('')
+
+const style = computed(() => {
+    return {
+        left: props.pos.left + 'px',
+        top: props.pos.top + 'px'
+    }
+})
+
+const handleCreateFolder = (title) => {
+    dialogTitle.value = title
+    isShowDialog.value = true
+}
+
+const handleCreateFile = (title) => {
+    dialogTitle.value = title
+    isShowDialog.value = true
+}
+
+const handleDelete = () => {
+
+}
+
+</script>
+
+
+<style scoped lang="scss">
+.tree-context-menu-box {
+    position: absolute;
+    background-color: white;
+    box-shadow: 0 0 5px 0px rgb(220, 220, 220);
+
+    .ctx-item {
+        width: 100%;
+        padding: 5px 10px;
+        border-radius: 5px;
+        text-align: center;
+        cursor: pointer;
+    }
+    .ctx-item:hover {
+        opacity: 0.5;
+    }
+}
+</style>
+
+
+<style scoped>
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+</style>
--- src/components/FileTree/index.vue
+++ src/components/FileTree/index.vue
@@ -1,23 +1,48 @@
 <template>
-    <div class="file-tree-box">
+    <div class="file-tree-box" ref="nodesRef" @click="handleClickFileTree">
         <h4>文件树</h4>
         <template v-if="props.data.length> 0">
-            <Nodes :nodes-data="props.data" @change-active-node="handleChangeActiveNode" :active-node="props.activeNode"></Nodes>
+            <Nodes  :nodes-data="props.data" @change-active-node="handleChangeActiveNode" :active-node="props.activeNode"></Nodes>
         </template>
+
+        <TreeContextMenu v-model:isShow="isShowContextMenu" :pos="pos" :active-node="props.activeNode"></TreeContextMenu>
     </div>
 </template>
 
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import Nodes from './components/Nodes.vue';
+import TreeContextMenu from './components/TreeContextMenu.vue';
 
 const props = defineProps(['data', 'activeNode'])
-
 const emits = defineEmits(['update:activeNode'])
+const isShowContextMenu = ref(false)
+const nodesRef = ref()
+const pos = ref({
+    left: 0,
+    top: 0
+})
 
 const handleChangeActiveNode = (newNode) => {
+    isShowContextMenu.value = false
     emits('update:activeNode', newNode)
 }
+
+const handleClickFileTree = () => {
+    isShowContextMenu.value = false
+    emits('update:activeNode', '')
+}
+
+onMounted(() => {
+  
+    nodesRef.value.oncontextmenu = (event) => {
+        event.preventDefault()
+        isShowContextMenu.value = true
+        pos.value.left = event.clientX - nodesRef.value.offsetLeft
+        pos.value.top = event.clientY - nodesRef.value.offsetTop
+    }
+})
 
 </script>
 
@@ -28,5 +53,6 @@ const handleChangeActiveNode = (newNode) => {
     width: 100%;
     padding: 10px;
     overflow: scroll;
+    position: relative;
 }
 </style>
